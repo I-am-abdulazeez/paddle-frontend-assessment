9d301c01e46bfbfe0bfa1e0a54b48fb51ac4023e
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-05-22 18:37:10 +0100

    chore: passed the newly added prop to Card component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,31 @@
-import { Center, VStack, Heading, HStack } from "@chakra-ui/react";
+import { Center, VStack, Heading, Stack, Text } from "@chakra-ui/react";
+
 import Card from "../components/Card";
 
 const Home: React.FC = () => {
   return (
-    <Center height={"100vh"}>
-      <VStack spacing={7}>
+    <Center height={"100vh"} p={{ base: 5, md: 0 }}>
+      <VStack spacing={10} textAlign="center">
         <Heading
-          size={"lg"}
+          size={"2xl"}
           bgClip="text"
           bgGradient="linear(to-l, #7928CA, #FF0080)"
         >
-          Welcome to Paddle-Frontend-Assessment
+          Welcome to <Text mt={3}>Paddle-Frontend-Assessment</Text>
         </Heading>
-        <HStack spacing={5}>
-          <Card cardButton="Go to task 1" cardText="Paddle Assessment Task 1" />
+        <Stack direction={["column", "row"]} spacing={5}>
           <Card
+            cardTopic="Design a landing page / Contact page"
+            cardButton="Go to task 1"
+            cardText="Paddle Assessment Task 1"
+          />
+          <Card
+            cardTopic="Use Github API to display latest repo from the last 30days"
             cardButton="Go to task 2"
             cardText="Paddle Assessment Task 2"
-            buttonColorScheme="pink"
+            cardButtonColorScheme="pink"
           />
-        </HStack>
+        </Stack>
       </VStack>
     </Center>
   );
