4a2c2a78bcd487ca0d1acb51cd2db8e5ea94e83f
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-05-22 06:41:12 +0100

    chore: Task-one page init

---
+++ src/pages/task-one.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
+const TaskOne: React.FC = () => {
+  return (
+    <Box>
+      <Text>Welcome to Task 1</Text>
+    </Box>
+  );
+};
+
+export default TaskOne;
