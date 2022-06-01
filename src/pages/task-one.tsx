858dc152174b8abd2bb9045b303eff117159fd35
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-01 19:51:45 +0100

    chore: getting task-one page ready,

--- src/pages/task-one.tsx
+++ src/pages/task-one.tsx
@@ -1,9 +1,21 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Container } from "@chakra-ui/react";
+import MainPage from "../components/MainPage";
+import Navbar from "../components/Navbar";
+import landingImage from "/src/images/landing.svg";
 
 const TaskOne: React.FC = () => {
   return (
-    <Box>
-      <Text>Welcome to Task 1</Text>
+    <Box
+      bgImage={`url(${landingImage})`}
+      bgPosition={"center"}
+      bgSize="cover"
+      p={3}
+      height="140vh"
+    >
+      <Navbar />
+      <Container maxWidth={"container.lg"}>
+        <MainPage />
+      </Container>
     </Box>
   );
 };
