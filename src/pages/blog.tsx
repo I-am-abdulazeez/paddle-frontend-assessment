56f560e110e51353ab526c02e5f5f7b07daf2956
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-02 04:15:47 +0100

    feat: blog pagewith simple heading

---
+++ src/pages/blog.tsx
@@ -0,0 +1,11 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+const Blog: React.FC = () => {
+  return (
+    <Box>
+      <Heading>Welcome to blog Page.</Heading>
+    </Box>
+  );
+};
+
+export default Blog;
