d85df679cbb6e31ecc65d668bc264a6fa257c53b
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-03 00:51:11 +0100

    chore: Added a function to fetchRepoData

--- src/contants/index.ts
+++ src/contants/index.ts
@@ -1,2 +1,9 @@
+import axios from "axios";
+
 export const GITHUB_API_URL =
   "https://api.github.com/search/repositories?q=created:>2022-05-02&sort=stars&order=desc&page=2";
+
+export const fetchRepoData = async () => {
+  const res = await axios.get(GITHUB_API_URL);
+  return res.data;
+};
