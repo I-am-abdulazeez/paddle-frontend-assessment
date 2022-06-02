b9f7956427f2cc6e5acdc3be1aca599a3698068e
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-02 04:02:09 +0100

    feat: Added a newprop called cardIsAllRounded to card card borderRadius based on it truthness

--- src/components/ClockCard/index.tsx
+++ src/components/ClockCard/index.tsx
@@ -2,17 +2,21 @@ import { Box, Heading, Text } from "@chakra-ui/react";
 
 import { ClockCardProps } from "../../interfaces";
 
-const ClockCard: React.FC<ClockCardProps> = ({ cardTag, cardTime }) => {
+const ClockCard: React.FC<ClockCardProps> = ({
+  cardTag,
+  cardTime,
+  cardIsAllRounded,
+}) => {
   return (
     <Box
       p={6}
       textAlign="center"
       bgColor={"white"}
       color="#000"
-      borderRadius="lg"
+      borderRadius={cardIsAllRounded ? "lg" : "0.5rem 0 0 0.5rem"}
     >
       <Heading>{cardTime}</Heading>
-      <Text>{cardTag}</Text>
+      <Text fontWeight={"medium"}>{cardTag}</Text>
     </Box>
   );
 };
