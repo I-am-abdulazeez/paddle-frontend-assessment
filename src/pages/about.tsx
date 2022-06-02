9dd0095e18825ebe67657bc08734b824dce96def
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-02 04:15:33 +0100

    feat: About page with simple heading

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+const About: React.FC = () => {
+  return (
+    <Box>
+      <Heading>Welcome to About us Page.</Heading>
+    </Box>
+  );
+};
+
+export default About;
