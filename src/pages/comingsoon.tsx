6ea8c7c326562dd7f64fdfe41bc2e3513a00faaa
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-02 03:29:12 +0100

    feat: Created a new Route /comingsoon and a new page Component.

---
+++ src/pages/comingsoon.tsx
@@ -0,0 +1,25 @@
+import { Box, Center, Container } from "@chakra-ui/react";
+import Footer from "../components/Footer";
+import MainPage from "../components/MainPage";
+import Navbar from "../components/Navbar";
+import landingImage from "/src/images/landing.svg";
+
+const ComingSoon: React.FC = () => {
+  return (
+    <Box
+      bgImage={`url(${landingImage})`}
+      bgPosition={"center"}
+      bgSize="cover"
+      p={3}
+      height={"140vh"}
+    >
+      <Navbar />
+      <Container maxWidth={"container.lg"}>
+        <MainPage />
+      </Container>
+      <Footer />
+    </Box>
+  );
+};
+
+export default ComingSoon;
