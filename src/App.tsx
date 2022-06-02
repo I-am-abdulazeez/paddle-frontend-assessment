4a8698d4dad8ddb4a4bbc61d80d21719332efbeb
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-02 04:15:17 +0100

    chore:  Addednew routes to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // Pages import
 import Home from "./pages";
+import About from "./pages/about";
+import Blog from "./pages/blog";
 import ComingSoon from "./pages/comingsoon";
 
 const App: React.FC = () => {
@@ -10,6 +12,8 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/comingsoon" element={<ComingSoon />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </Router>
   );
