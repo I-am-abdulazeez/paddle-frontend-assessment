2f9b7176d3bac00dea2b97dfc9670cf902c390ef
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-03 06:16:17 +0100

    chore: Added new function called kFormatter to fomat 1000 to 1k

--- src/contants/index.ts
+++ src/contants/index.ts
@@ -7,3 +7,11 @@ export const fetchRepoData = async () => {
   const res = await axios.get(GITHUB_API_URL);
   return res.data;
 };
+
+export const kFormatter = (num: number) => {
+  return num > 999
+    ? num % 1000 === 0
+      ? (num / 1000).toFixed(0) + "k"
+      : (num / 1000).toFixed(1) + "k"
+    : num;
+};
