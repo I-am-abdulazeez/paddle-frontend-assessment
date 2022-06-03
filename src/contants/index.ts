692e8db7574dde379cdb3b33f944e08e1ae36e32
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-03 06:25:32 +0100

    chore: connected url from .env file

--- src/contants/index.ts
+++ src/contants/index.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-export const GITHUB_API_URL =
-  "https://api.github.com/search/repositories?q=created:%3E2022-05-02&sort=stars&order=desc";
+export const GITHUB_API_URL = import.meta.env.VITE_GITHUB_API_URL;
 
 export const fetchRepoData = async () => {
-  const res = await axios.get(GITHUB_API_URL);
+  const res = await axios.get(
+    `${GITHUB_API_URL}q=created:%3E2022-05-02&sort=stars&order=desc`
+  );
   return res.data;
 };
 
