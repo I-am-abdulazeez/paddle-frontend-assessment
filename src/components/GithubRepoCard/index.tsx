3d60b8e48818bb500a731a82d681448b86e8e336
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-03 01:30:59 +0100

    chore: more-ui design on GithubRepoCard component

--- src/components/GithubRepoCard/index.tsx
+++ src/components/GithubRepoCard/index.tsx
@@ -1,23 +1,47 @@
-import { HStack, Image } from "@chakra-ui/react";
+import {
+  Badge,
+  Button,
+  Heading,
+  HStack,
+  Image,
+  Stack,
+  Text,
+  useBreakpointValue,
+  VStack,
+} from "@chakra-ui/react";
 import { GithubRepoCardProps } from "../../interfaces";
 
 const GithubRepoCard: React.FC<GithubRepoCardProps> = ({ item }) => {
+  const headingSize = useBreakpointValue({ base: "sm", sm: "md" });
   return (
     <HStack
       p={4}
       borderRadius="lg"
       border="1px solid #EDF2F7"
       shadow="sm"
-      my={5}
-      key={item.id}
+      my={7}
+      spacing={8}
     >
       <Image
         boxSize="70px"
         objectFit="cover"
+        border="1px solid #EDF2F7"
         borderRadius="full"
         src={item?.owner?.avatar_url}
         alt={item?.full_name}
       />
+      <Stack spacing={3}>
+        <Heading size={headingSize}>{item.name}</Heading>
+        <Text>{item?.description}</Text>
+        <HStack spacing={4}>
+          <Badge fontSize={"md"} colorScheme="purple">
+            Stars: {item?.stargazers_count}
+          </Badge>
+          <Badge fontSize={"md"} colorScheme="pink">
+            Stars: {item?.open_issues_count}
+          </Badge>
+        </HStack>
+      </Stack>
     </HStack>
   );
 };
