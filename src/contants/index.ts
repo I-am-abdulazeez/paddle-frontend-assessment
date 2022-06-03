ddd09a3b2765d4caa57f93bf9b2e8afbd612c634
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-03 01:29:41 +0100

    chore: updated api_url

--- src/contants/index.ts
+++ src/contants/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const GITHUB_API_URL =
-  "https://api.github.com/search/repositories?q=created:>2022-05-02&sort=stars&order=desc&page=2";
+  "https://api.github.com/search/repositories?q=created:%3E2022-05-02&sort=stars&order=desc";
 
 export const fetchRepoData = async () => {
   const res = await axios.get(GITHUB_API_URL);
