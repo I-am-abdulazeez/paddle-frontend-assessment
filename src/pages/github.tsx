a3b841670a4b2b8d1d605d63236db522b0da5efa
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-03 09:22:14 +0100

    chore: implemented infiniteQuery to fetch data and also added loadMore button to load more data.

--- src/pages/github.tsx
+++ src/pages/github.tsx
@@ -1,35 +1,78 @@
 import {
   Box,
+  Button,
   Center,
   Container,
   Heading,
-  HStack,
-  Image,
   Spinner,
   Text,
 } from "@chakra-ui/react";
+import { MouseEventHandler } from "react";
 
-import { useQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
 import GithubRepoCard from "../components/GithubRepoCard";
 import { fetchRepoData } from "../contants";
 
 const Github: React.FC = () => {
-  const { data, isLoading } = useQuery("repoData", fetchRepoData);
-  console.log(data?.items);
+  const { data, isError, isLoading, isFetching, fetchNextPage, hasNextPage } =
+    useInfiniteQuery(
+      ["repoData"],
+      ({ pageParam = 1 }) => fetchRepoData(pageParam),
+      {
+        // for infinite Query
+        keepPreviousData: true,
+        onSuccess: () => console.log("data fetch with no problemo!"),
+        onError: () => console.log("data fetch error!"),
+        getNextPageParam: (lastPage, allPages) => {
+          // get the max page first
+          const maxPages = lastPage?.total_count / 30;
+          const nextPage = allPages?.length + 1;
+          return nextPage <= maxPages ? nextPage : undefined;
+        },
+      }
+    );
+
+  const handleLoadMore:
+    | MouseEventHandler<HTMLButtonElement>
+    | undefined = () => {
+    if (hasNextPage) {
+      fetchNextPage();
+    }
+  };
+
   return (
-    <Container maxW="container.lg">
-      <Heading my={10} size={"lg"} textAlign={"center"}>
-        Trending Github Repos 💪💪
-      </Heading>
-      <Center mt={4}>
-        {isLoading && (
-          <Spinner thickness="3px" emptyColor="gray.200" color="pink.500" />
-        )}
-      </Center>
-      {data?.items?.map((repo: any) => (
-        <GithubRepoCard repo={repo} key={repo.id} />
-      ))}
-    </Container>
+    <Box py={6}>
+      <Container maxW="container.lg">
+        <Heading my={10} size={"lg"} textAlign={"center"}>
+          Trending Github Repos in the last 30 Days 💪💪💪
+        </Heading>
+
+        <Center mt={4}>
+          {isFetching && (
+            <Spinner thickness="3px" emptyColor="gray.200" color="pink.500" />
+          )}
+        </Center>
+
+        <Center mt={4}>{isError && <Text>Error loading data</Text>}</Center>
+        {data?.pages?.map((group: any) => {
+          return group?.items?.map((repo: any) => {
+            return <GithubRepoCard repo={repo} key={repo.id} />;
+          });
+        })}
+        <Center mt={4}>
+          {!isLoading && (
+            <Button
+              colorScheme={"purple"}
+              isLoading={isFetching || !hasNextPage}
+              size={"sm"}
+              onClick={handleLoadMore}
+            >
+              Load More
+            </Button>
+          )}
+        </Center>
+      </Container>
+    </Box>
   );
 };
 
