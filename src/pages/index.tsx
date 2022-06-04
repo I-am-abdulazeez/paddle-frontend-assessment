8c35b91425befc84dd3b1c4f341c58ff3ba73e7b
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-04 18:21:22 +0100

    fix: typo error in homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,13 +17,13 @@ const Home: React.FC = () => {
           <Card
             cardTopic="Design a  Comming soon page / Conatct Us page"
             cardButton="Go to coming soon page"
-            cardText="Paddle Assessment Task 1"
+            cardText="Peddle Assessment Task 1"
             cardLinkTo="/comingsoon"
           />
           <Card
             cardTopic="Use Github API to display latest repo from the last 30days"
             cardButton="Go to github data page"
-            cardText="Paddle Assessment Task 2"
+            cardText="Peddle Assessment Task 2"
             cardButtonColorScheme="pink"
             cardLinkTo="/github"
           />
